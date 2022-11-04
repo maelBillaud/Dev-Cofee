91545a9e1eaf55bd6b441c39e3bcc2cff6478ab8
Maël Billaud <billaud.mael@gmail.com>
2022-11-04 15:13:22 +0100

    renommage des paths

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("sw.js")
       .then(res => console.log("Enregistrement du service worker réussi"))
       .catch(err => console.log("Échec de l'enregistrement du service worker", err))
   })
@@ -9,15 +9,15 @@ if ("serviceWorker" in navigator) {
 
 const container = document.querySelector(".container");
 const coffees = [
-  { name: "Perspiciatis", image: "/images/coffee1.jpg" },
-  { name: "Voluptatem", image: "/images/coffee2.jpg" },
-  { name: "Explicabo", image: "/images/coffee3.jpg" },
-  { name: "Rchitecto", image: "/images/coffee4.jpg" },
-  { name: "Beatae", image: "/images/coffee5.jpg" },
-  { name: "Vitae", image: "/images/coffee6.jpg" },
-  { name: "Inventore", image: "/images/coffee7.jpg" },
-  { name: "Veritatis", image: "/images/coffee8.jpg" },
-  { name: "Accusantium", image: "/images/coffee9.jpg" },
+  { name: "Perspiciatis", image: "images/coffee1.jpg" },
+  { name: "Voluptatem", image: "images/coffee2.jpg" },
+  { name: "Explicabo", image: "images/coffee3.jpg" },
+  { name: "Rchitecto", image: "images/coffee4.jpg" },
+  { name: "Beatae", image: "images/coffee5.jpg" },
+  { name: "Vitae", image: "images/coffee6.jpg" },
+  { name: "Inventore", image: "images/coffee7.jpg" },
+  { name: "Veritatis", image: "images/coffee8.jpg" },
+  { name: "Accusantium", image: "images/coffee9.jpg" },
 ];
 
 const showCoffees = () => {
